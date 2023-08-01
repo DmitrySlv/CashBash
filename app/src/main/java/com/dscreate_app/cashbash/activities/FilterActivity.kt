d9812eefc303dd8648759944758a228ddd7462ec
DmitrySlv <dscreateapp@gmail.com>
2023-08-01 17:58:31 +0400

    Создание логики для формирования фильтра в виде строки.
    Добавление в разметку activity_filter.xml нужных пунктов для экрана фильтрации.
    
    В FilterActivity.kt добавление функций для открытия диалога при нажатии на кнопку страны и города в фильтрах. И создание слушателя нажатий на кнопку отфильтровать.
    
    Создание private fun createFiler(): String = with(binding) - где с помощью StringBuilder() создается временный лист с заполненными полями из филтра. И с помощью цикла for() по индексу и string проверяется строка на значения по умолч и на не пустую строку и добавляется. И проверка на достижения массива из 3-х значений, то не добавлять "_".

--- app/src/main/java/com/dscreate_app/cashbash/activities/FilterActivity.kt
+++ app/src/main/java/com/dscreate_app/cashbash/activities/FilterActivity.kt
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.dscreate_app.cashbash.R
 import com.dscreate_app.cashbash.databinding.ActivityFilterBinding
+import com.dscreate_app.cashbash.utils.dialogs.CityHelper
+import com.dscreate_app.cashbash.utils.dialogs.DialogSpinnerHelper
+import com.dscreate_app.cashbash.utils.logD
+import com.dscreate_app.cashbash.utils.showToast
 
 class FilterActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityFilterBinding.inflate(layoutInflater) }
+    private val dialog = DialogSpinnerHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initToolBar()
+        onClickSelectCountry()
+        onClickSelectCity()
+        onClickFilterDone()
     }
 
     private fun initToolBar() = with(binding) {
@@ -29,5 +37,61 @@ class FilterActivity : AppCompatActivity() {
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun onClickSelectCountry() = with(binding) {
+        tvSelectCountry.setOnClickListener {
+            val listCountry = CityHelper.getAllCountries(this@FilterActivity)
+            dialog.showSpinnerDialog(this@FilterActivity, listCountry, tvSelectCountry)
+            if (tvSelectCity.text.toString() != getString(R.string.select_city)) {
+                tvSelectCity.text = getString(R.string.select_city)
+            }
+        }
+    }
+
+    private fun onClickSelectCity() = with(binding) {
+        tvSelectCity.setOnClickListener {
+            val selectedCountry = tvSelectCountry.text.toString()
+            if (selectedCountry != getString(R.string.select_country)) {
+                val listCity = CityHelper.getAllCities(
+                    this@FilterActivity, selectedCountry
+                )
+                dialog.showSpinnerDialog(this@FilterActivity, listCity, tvSelectCity)
+            } else {
+                showToast(getString(R.string.no_select_country))
+            }
+        }
+    }
+
+    private fun onClickFilterDone() = with(binding) {
+        btFilter.setOnClickListener {
+            logD(TAG, "Filter: ${createFiler()}")
+        }
+    }
+
+    private fun createFiler(): String = with(binding) {
+        val sBuilder = StringBuilder()
+        val listTempFilter = listOf(
+            tvSelectCountry.text,
+            tvSelectCity.text,
+            edIndex.text,
+            cbWithSend.isChecked.toString()
+            )
+        for ((index, string) in listTempFilter.withIndex()) {
+            if (string != getString(R.string.select_country) &&
+                string != getString(R.string.select_city) &&
+                string.isNotEmpty()
+                ) {
+                sBuilder.append(string)
+                if (index != listTempFilter.size - 1) {
+                    sBuilder.append("_")
+                }
+            }
+        }
+        return sBuilder.toString()
+    }
+
+    companion object {
+        private const val TAG = "MyLog"
     }
 }
