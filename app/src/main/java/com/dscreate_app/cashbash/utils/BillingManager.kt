0ab552e45fa7d66fb87409ae9651136f643145ba
DmitrySlv <dscreateapp@gmail.com>
2023-08-09 17:11:38 +0400

    Встроенные покупки. Ч2.
    Создание функции получения покупки private fun getItem() и запуск её в override fun onBillingSetupFinished(p0: BillingResult) {}.

--- app/src/main/java/com/dscreate_app/cashbash/utils/BillingManager.kt
+++ app/src/main/java/com/dscreate_app/cashbash/utils/BillingManager.kt
@@ -2,12 +2,17 @@ package com.dscreate_app.cashbash.utils
 
 import androidx.appcompat.app.AppCompatActivity
 import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.BillingResult
 import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.SkuDetailsParams
 
 class BillingManager(private val act: AppCompatActivity) {
     private var billingClient: BillingClient? = null
 
     init {
+        setUpBillingClient()
     }
 
     private fun setUpBillingClient() {
@@ -24,5 +29,38 @@ class BillingManager(private val act: AppCompatActivity) {
                 }
             }
         }
+    }
+
+    fun startConnection() {
+        billingClient?.startConnection(object : BillingClientStateListener {
+            override fun onBillingServiceDisconnected() {
+            }
+
+            override fun onBillingSetupFinished(p0: BillingResult) {
+                getItem()
+            }
+        })
+    }
+
+    private fun getItem() {
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_ADS)
+        val skuDetails = SkuDetailsParams.newBuilder()
+        skuDetails.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        billingClient?.querySkuDetailsAsync(skuDetails.build()) { result, list ->
+            run {
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                    if (!list.isNullOrEmpty()) {
+                        val billingFlowParams = BillingFlowParams.newBuilder()
+                            .setSkuDetails(list[0]).build()
+                        billingClient?.launchBillingFlow(act, billingFlowParams)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val REMOVE_ADS = "remove_ads"
     }
 }
